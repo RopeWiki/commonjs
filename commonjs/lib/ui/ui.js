863f89337059a093f654e45e9a518330149d53de
Michelle Nilles <45955186+catware2@users.noreply.github.com>
2020-11-30 21:36:26 -0800

    Don't reload page on weather check

--- commonjs/lib/ui/ui.js
+++ commonjs/lib/ui/ui.js
@@ -8,9 +8,10 @@ function toggleUrlcheckbox(elem) {
 
 function togglewchk(varname) {
     var varval = !eval(varname);
-    setCookie(varname, varval ? "on" : "", 360*10); // 10 years
-    document.body.style.cursor = 'wait';
-    window.location.reload();
+    weather = varval ? "on" : "";
+    setCookie(varname, weather, 360*10); // 10 years
+
+    loadInlineWeather();
 }
 
 function toggleStarrate(force) {
@@ -770,12 +771,11 @@ function loadInlineWeather() {
         //May2018 WUnderground API was shutdown
         //Aug2020 DarkSky API was shutdown (purchased by Apple)
         //so use OpenWeather (limit with free account 60 calls/minute)
-        var openweatherApiKey = "1d5f0c74f9119e20765fed256ecfadc5";
 
-        var url = "http://api.openweathermap.org/data/2.5/onecall?lat=" + coords[0] + "&lon=" + coords[1] + "&exclude=current,minutely,hourly&appid=" + openweatherApiKey;
+        var url = "http://api.openweathermap.org/data/2.5/onecall?lat=" + coords[0] + "&lon=" + coords[1] + "&exclude=current,minutely,hourly&appid=" + OPENWEATHER_APIKEY;
 
         $.getJSON(geturl(url),
-            function(data) {
+            function (data) {
                 // dailyforecast
                 if (data &&
                     data.daily &&
@@ -806,7 +806,7 @@ function loadInlineWeather() {
                     w += '<span class="wstheader units" style="float:right;">' + (metric ? "&#176;C" : "&#7506;F") + '</span>';
 
                     //w += '<img class="wstlogo" />';
-                    
+
                     w += '</span>';
 
                     w += '</div>';
@@ -834,7 +834,7 @@ function loadInlineWeather() {
                         var date = new Date(periods[i].dt * 1000);
 
                         w += '<td title="' + date.getDate() + ' ' + months[date.getMonth()] + ' : &quot;' + periods[i].weather[0].description + '&quot; Max ' + h + (metric ? 'C' : 'F') + ' Min ' + l + (metric ? 'C' : 'F') + '">';
-                        var weatherIconSize = periods[i].weather[0].icon === "01d" ? "30" : "40" ; //if it's the sun, shrink the size down a little more 
+                        var weatherIconSize = periods[i].weather[0].icon === "01d" ? "30" : "40"; //if it's the sun, shrink the size down a little more 
                         w += '<div class="weatherimg" style="background-image: url(&#39;http://openweathermap.org/img/wn/' + periods[i].weather[0].icon + '.png&#39;);  background-size: ' + weatherIconSize + 'px ' + weatherIconSize + 'px;"/></div>';
                         w += '<div class="weatherh">' + h + '</div>';
                         w += '<div class="weatherl">' + l + '</div>';
@@ -855,10 +855,15 @@ function loadInlineWeather() {
                         w += '</td></tr>';
                     }
                     w += '</table>';
-                    
+
                     weatherdiv.innerHTML = w;
                 }
             });
+    } else {
+        if (weatherdiv)
+        //weatherdiv.style.display = "none";
+            weatherdiv.innerHTML =
+                '<a rel="nofollow" class="external text" href="http://ropewiki.com/Weather?location=Lomatium+%26+Krill+Loop"><img alt="Wforecast.png" src="/images/d/d5/Wforecast.png" width="13" height="22"> Weather forecast</a>';
     }
 }
 
