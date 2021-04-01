f065190cd7fa7e56b86a2ec8805eee194dc4cb21
Michelle Nilles <45955186+catware2@users.noreply.github.com>
2021-04-01 15:01:21 -0700

    Update zzz_onload.js

--- commonjs/lib/zzz_onload.js
+++ commonjs/lib/zzz_onload.js
@@ -1,12 +1,12 @@
 // Run these actions upon load (after all the functions have been defined)
 
+// required for CSS for small screens
+$('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+
 initializeCookies();
 initializeGlobalVariables();
 addPopOutLinkSupport();
 addUACAStyle();
-
-// required for CSS for small screens
-$('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
 
 //first heading text
 setHeadingText();
