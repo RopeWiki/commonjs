446b00ebea1050e7d21492b56c19c574ba2c59cf
Michelle Nilles <45955186+catware2@users.noreply.github.com>
2021-07-28 12:55:32 -0700

    Update constants.js

--- commonjs/lib/constants.js
+++ commonjs/lib/constants.js
@@ -1,14 +1,33 @@
-// Hostname of server hosting the RWServer server (see https://github.com/RopeWiki/RWServer).
-const LUCA_HOSTNAME = "luca.ropewiki.com";
+//env: 1-prod, 2-dev, 3-proddev
+const env = 1;
 
 // communication protocol
-const PROTOCOL = "https://";
+var PROTOCOL;
 
-// Base URl of the RWServer server.
-const LUCA_BASE_URL = PROTOCOL + LUCA_HOSTNAME;
+// primary hostname of the site.
+var SITE_HOSTNAME;
 
-// Primary hostname of the site.
-const SITE_HOSTNAME = "ropewiki.com";
+// Google Maps API key
+var GOOGLE_MAPS_APIKEY;
+
+switch (env) {
+case 1:
+        PROTOCOL = "http://";
+        SITE_HOSTNAME = "ropewiki.com";
+        //GOOGLE_MAPS_APIKEY = "AIzaSyDdkcexZV-p5Nj8RwgLYTcegm5jorJpbyw"; //ben's
+        GOOGLE_MAPS_APIKEY = "AIzaSyCzx6LOfuFbI0ZpdoEKKvf77EO8-YXP_Cw"; //public (mine)
+        break;
+case 2:
+        PROTOCOL = "http://";
+        SITE_HOSTNAME = "192.168.1.40:8080";
+        GOOGLE_MAPS_APIKEY = "";
+        break;
+case 3:
+        PROTOCOL = "https://";
+        SITE_HOSTNAME = "dev.ropewiki.com";
+        GOOGLE_MAPS_APIKEY = "AIzaSyCzx6LOfuFbI0ZpdoEKKvf77EO8-YXP_Cw"; //public (mine)
+        break;
+}
 
 // Base URL of the site (from which all resources are descended).
 const SITE_BASE_URL = PROTOCOL + SITE_HOSTNAME;
@@ -18,8 +37,10 @@ const SITE_ALTERNATE_NAMES = [
     PROTOCOL + "www.ropewiki.com"
 ];
 
-// Google Maps API key
-const GOOGLE_MAPS_APIKEY = "AIzaSyDdkcexZV-p5Nj8RwgLYTcegm5jorJpbyw"; //ben's
+// Hostname of server hosting the RWServer server (see https://github.com/RopeWiki/RWServer).
+const LUCA_HOSTNAME = "luca.ropewiki.com";
 
+// Base Url of the RWServer server.
+const LUCA_BASE_URL = PROTOCOL + LUCA_HOSTNAME;
 
 const OPENWEATHER_APIKEY = "1d5f0c74f9119e20765fed256ecfadc5";
