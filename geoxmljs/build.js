e638d30234e28defc748fb3502ddb9665ceb9657
coops <coops@fawk.eu>
2025-03-29 22:46:21 -0700

    Setup geoxmljs build.js
    This is my first time touching geoxmljs, and I wanted to make it build the same way as commonjs:
     - copy over build.js file and tweak output filename
     - delete bundleconfig.json which seems redundant (correct me if i'm wrong)
     - add out/ for git tracking of the generated output
    
    I did "cd geoxmljs; ln -s ../commonjs/node_modules ./" to avoid two node setups.

---
+++ geoxmljs/build.js
@@ -0,0 +1,48 @@
+var UglifyJS = require("uglify-js");
+var fs = require('fs');
+var path = require('path');
+
+// Note: enabling `compress` breaks functionality on ropewiki.com currently
+var options = {
+    "compress": false,
+    "mangle": false,
+    "warnings": true,
+    "output": {
+        "preamble": "// Do not modify this page directly; instead build from https://github.com/RopeWiki/commonjs",
+        "beautify": true
+    }
+};
+
+const inputFiles = {}
+function addInputFilesFrom(inputPath) {
+    fs.readdirSync(inputPath).forEach(filename => {
+        const filepath = path.resolve(inputPath, filename);
+        const stat = fs.statSync(filepath);
+        const isFile = stat.isFile();
+
+        if (isFile) {
+            inputFiles[filepath] = fs.readFileSync(filepath, "utf8");
+        } else {
+            addInputFilesFrom(filepath);
+        }
+    });
+}
+addInputFilesFrom(path.join(__dirname, 'lib'));
+
+const result = UglifyJS.minify(inputFiles, options);
+
+if (result.warnings != null) {
+    console.log("===== Warnings =====")
+    console.log(result.warnings);
+} else {
+    console.log("No warnings.")
+}
+
+if (result.error != null) {
+    console.log("===== Errors =====")
+    console.log(result.error);
+} else {
+    console.log("No errors.")
+}
+
+fs.writeFileSync("out/Geoxml.min.js", result.code, "utf8");
